0018c5a6d348a788d9423a21211ecb3db5a03c70
Carlos <carlosiribar@gmail.com>
2016-03-31 19:55:33 -0300

    First Skeleton React App

---
+++ src/components/note.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => (<div className='red'> Hello World! </div>);
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import './main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Note from './components/note.jsx';
+
+ReactDOM.render(<Note/>, document.getElementById('app'));
